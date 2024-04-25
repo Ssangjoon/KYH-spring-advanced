fe3c8af8a9fc41762a5252900870c56a7219f1c3
ssang <qkqh34k@gmail.com>
2024-04-25 14:09:40 +0900

    ThreadLocalServiceTest

--- advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -1,35 +1,33 @@
 package hello.advanced.trace.threadlocal;
 
-import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
-    private FieldService fieldService = new FieldService();
+public class ThreadLocalServiceTest {
+    private ThreadLocalService service = new ThreadLocalService();
 
     @Test
-    void field() {
+    void threadLocal(){
         log.info("main start");
         Runnable userA = () -> {
-            fieldService.logic("userA");
+            service.logic("userA");
         };
         Runnable userB = () -> {
-            fieldService.logic("userB");
+            service.logic("userB");
         };
-
         Thread threadA = new Thread(userA);
         threadA.setName("thread-A");
         Thread threadB = new Thread(userB);
         threadB.setName("thread-B");
-
-        threadA.start(); //A실행
-//        sleep(2000); //동시성 문제 발생X
-        sleep(100); //동시성 문제 발생O
-        threadB.start(); //B실행
-        sleep(3000); //메인 쓰레드 종료 대기
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(2000);
         log.info("main exit");
     }
+
     private void sleep(int millis) {
         try {
             Thread.sleep(millis);
