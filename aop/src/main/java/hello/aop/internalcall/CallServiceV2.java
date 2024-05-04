6813047bca86e924a2f12d8093a51a630e82aaa1
SangJoon Lee <qkqh34k@gmail.com>
2024-05-05 00:54:00 +0900

    프록시와 내부 호출 - 대안

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,36 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * ObjectProvider(Provider), ApplicationContext를 사용해서 지연(LAZY) 조회
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV2 {
+    //private final ApplicationContext applicationContext;
+
+//    public CallServiceV2(ApplicationContext applicationContext) {
+//        this.applicationContext = applicationContext;
+//    }
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+
+    @Autowired
+    public void external(){
+        log.info("call external");
+        //CallServiceV2 callServiceV2 = applicationContext.getBean(CallServiceV2.class);
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
