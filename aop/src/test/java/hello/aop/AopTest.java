81c36d9e706fae38eda14f1e58f163788a78bdbc
SangJoon Lee <qkqh34k@gmail.com>
2024-05-04 12:42:36 +0900

    어드바이스 순서와 종류

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,9 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
-import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -17,7 +15,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 @Slf4j
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+//@Import(AspectV5Order.class)
+@Import(AspectV6Advice.class)
 @SpringBootTest
 public class AopTest {
     @Autowired
